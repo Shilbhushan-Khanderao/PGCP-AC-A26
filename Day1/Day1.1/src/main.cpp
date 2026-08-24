02b8e13de718c8102116b0deb302fcea538b751a
Shilbhushan-Khanderao <kshilbhushan@gmail.com>
2026-08-24 15:07:23 +0530

    Day 1 content

---
+++ Day1/Day1.1/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>	// include directive: iostream header file
+
+int main(){
+
+	int a;
+	int b;
+
+	std::cout << "Enter two numbers:\n";
+	std::cin >> a >> b;
+
+
+	std::cout << "SUM = " << a + b;
+
+	return 0;
+}
+
+
+
+
+
+
